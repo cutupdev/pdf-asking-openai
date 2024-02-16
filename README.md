6b9553fd8614f72fc3609eb1c0836e2313220c95
catlover75926 <takasi75926@gmail.com>
2024-02-16 19:39:32 +0100

    Feat complete

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# pdf-asking-openai
-This is pdf upload-ask-answer project using openai, streamlit.
+# ChatPDF
+
+Chat with any PDF. 
+
+Easily upload the PDF documents you'd like to chat with. Instant answers. Ask questions, extract information, and summarize documents with AI. Sources included.
+
+Create app like [ChatPDF](https://www.chatpdf.com/) or [PDF.ai](https://pdf.ai/) in less than 10 lines of code
+
+### Getting Started
+
+Code is up now, ⭐ (Star) the repo to receive updates
+
+Replit and streamlit version coming soon
+
+### How to run ? (Things might change based on OS)
+
+1. Create a virtual environment in python https://docs.python.org/3/library/venv.html
+
+2. Run "pip install -r requirements.txt"
+
+3. Set OPENAI_API_KEY environment variable with your openai key
+
+4. Run "python main.py"
+
+5. Change pdf file and query in code if you want to try with any other content
+
+To run streamlit app, follow the steps run "streamlit run streamlitui.py"
+
+Parts of the streamlit code is inspired from [here](https://github.com/catlover75926/pdf-asking-openai)
--- app.py
+++ app.py
@@ -13,37 +13,10 @@ import os
 
 st.set_page_config(page_title='🤗💬 PDF Chat App - GPT')
 
-# # Sidebar contents
-# with st.sidebar:
-#     st.title('🤗💬 PDF Chat App')
-#     st.markdown('''
-#     ## About
-#     This app is an LLM-powered chatbot built using:
-#     - [Streamlit](https://streamlit.io/)
-#     - [LangChain](https://python.langchain.com/)
-#     - [OpenAI](https://platform.openai.com/docs/models) LLM model
-
-#     ''')
-#     add_vertical_space(5)
-#     st.write('Made with ❤️ by [Livia Ellen](https://liviaellen.com/portfolio)')
-
-
 
 
 def main():
-    # st.header("Talk to your PDF 💬")
-    # st.write("This app uses OpenAI's LLM model to answer questions about your PDF file. Upload your PDF file and ask questions about it. The app will return the answer from your PDF file.")
-
-    # st.header("1. Pass your OPEN AI API KEY here")
-    # v='demo'
-    # openai_key=st.text_input("**OPEN AI API KEY**", value=v)
-    # st.write("You can get your OpenAI API key from [here](https://platform.openai.com/account/api-keys)")
-
-
-    # if openai_key==v:
-    #     openai_key=st.secrets["OPENAI_API_KEY"]
-    # if openai_key=='':
-    #     load_dotenv()
+    
     openai_key = "your key"
     os.environ["OPENAI_API_KEY"] = openai_key
 
@@ -51,8 +24,6 @@ def main():
 
     st.header("1. Upload PDF")
     pdf = st.file_uploader("**Upload your PDF**", type='pdf')
-
-    # st.write(pdf)
 
     if pdf is not None:
         pdf_reader = PdfReader(pdf)
@@ -68,37 +39,24 @@ def main():
             )
         chunks = text_splitter.split_text(text=text)
 
-        # # embeddings
+        # embeddings
         store_name = pdf.name[:-4]
         st.write(f'{store_name}')
-        # st.write(chunks)
 
         if os.path.exists(f"{store_name}.pkl"):
             with open(f"{store_name}.pkl", "rb") as f:
                 VectorStore = pickle.load(f)
-            # st.write('Embeddings Loaded from the Disk')s
         else:
             embeddings = OpenAIEmbeddings()
             VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
             with open(f"{store_name}.pkl", "wb") as f:
                 pickle.dump(VectorStore, f)
 
-        # embeddings = OpenAIEmbeddings()
-        # VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-
-    # st.header("or.. Try it with this The Alchaemist PDF book")
-    # if st.button('Ask The Alchemist Book Questions'):
-    #     with open("The_Alchemist.pkl", "rb") as f:
-    #         VectorStore = pickle.load(f)
-        # Accept user questions/query
         st.header("2. Ask questions about your PDF file:")
         q="Tell me about the content of the PDF"
         query = st.text_input("Questions",value=q)
-        # st.write(query)
 
         if st.button("Ask"):
-            # st.write(openai_key)
-            # os.environ["OPENAI_API_KEY"] = openai_key
             if openai_key=='':
                 st.write('Warning: Please pass your OPEN AI API KEY on Step 1')
             else:
